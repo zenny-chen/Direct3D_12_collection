9f618b84e6988e3ea8b9de08a5a6956cffd6ae01
Zenny Chen <zenny_chen@sina.com>
2024-02-18 01:09:34 +0800

    Add files via upload

--- Direct3D_12_collection/Direct3D_12_collection/shaders/gs_test.ps.hlsl
+++ Direct3D_12_collection/Direct3D_12_collection/shaders/gs_test.ps.hlsl
@@ -1,10 +1,11 @@
-struct PSInput
+struct GSOutType
 {
     float4 position : SV_POSITION;
     nointerpolation float4 color : COLOR;
+    uint viewportIndex : SV_ViewportArrayIndex;
 };
 
-float4 PSMain(PSInput input) : SV_TARGET
+float4 PSMain(GSOutType input) : SV_TARGET
 {
     return input.color;
 }
